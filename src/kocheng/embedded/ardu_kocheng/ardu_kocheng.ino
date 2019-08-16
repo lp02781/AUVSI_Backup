eede8ab6a8ee80f42fb02951b0ca5b7a594ed560
lp02781 <lp02781@gmail.com>
2019-08-16 13:37:29 +0700

    finish

--- src/kocheng/embedded/ardu_kocheng/ardu_kocheng.ino
+++ src/kocheng/embedded/ardu_kocheng/ardu_kocheng.ino
@@ -17,8 +17,11 @@ const int trigPin_5 = 11;
 const int echoPin_5 = 12;
 
 int pwm_remote;
-int middle_pwm = 120;
+int pwm_remote_2;
+int middle_pwm = 1300;
+int middle_analog = 130;
 int remote_pin = A3;
+int remote_pin_2 = A4;
 
 int led_red     = A0;
 int led_yellow  = A1;
@@ -57,6 +60,7 @@ void setup()
   pinMode(led_yellow, OUTPUT);
 
   pinMode(remote_pin, INPUT);
+  pinMode(remote_pin_2, INPUT);
   
   pinMode(trigPin_1, OUTPUT); 
   pinMode(echoPin_1, INPUT);
@@ -67,6 +71,7 @@ void setup()
   pinMode(trigPin_4, OUTPUT); 
   pinMode(echoPin_4, INPUT);
 
+  //Serial.begin(9600);
 }
 
 void loop()
@@ -74,6 +79,7 @@ void loop()
   nh.spinOnce();
 
   pwm_remote = analogRead(remote_pin);
+  pwm_remote_2 = pulseIn(remote_pin_2, HIGH);
   
   digitalWrite(trigPin_1, LOW);
   digitalWrite(trigPin_2, LOW);
@@ -106,6 +112,12 @@ void loop()
   distance_3= duration_3*0.034/2;
   distance_4= duration_4*0.034/2;
   distance_5= duration_5*0.034/2;
+
+  if(distance_1 = 0){distance_1 = 1;}
+  if(distance_2 = 0){distance_2 = 1;}
+  if(distance_3 = 0){distance_3 = 1;}
+  if(distance_4 = 0){distance_4 = 1;}
+  if(distance_5 = 0){distance_5 = 1;}
   
   srf_msg.data[0]= distance_1;
   srf_msg.data[1]= distance_2;
@@ -113,27 +125,32 @@ void loop()
   srf_msg.data[3]= distance_4;
   srf_msg.data[4]= distance_5;
   
-  chatter.publish( &srf_msg );
+  //chatter.publish( &srf_msg );
   
-  if(kocheng_status==1 && pwm_remote<middle_pwm){//manual
-    digitalWrite(led_red, LOW);
-    digitalWrite(led_yellow, HIGH);
-    digitalWrite(led_green, LOW);    
-  }
-  else if(kocheng_status==2 && pwm_remote<middle_pwm){//auto
-    digitalWrite(led_red, LOW);
-    digitalWrite(led_yellow, LOW);
-    digitalWrite(led_green, HIGH);
-  }
-  else if(pwm_remote>middle_pwm && kocheng_status!=2 &&kocheng_status!=1){//motor
+  if(pwm_remote>middle_analog){//motor
     digitalWrite(led_red, HIGH);
     digitalWrite(led_yellow, LOW);
-    digitalWrite(led_green, LOW);      
+    digitalWrite(led_green, LOW);    
   }
   else {
-    digitalWrite(led_red, LOW);
-    digitalWrite(led_yellow, LOW);
-    digitalWrite(led_green, LOW); 
+    if(pwm_remote_2>middle_pwm){//auto
+      digitalWrite(led_red, LOW);
+      digitalWrite(led_yellow, LOW);
+      digitalWrite(led_green, HIGH);
+    }
+    else {//motor
+      digitalWrite(led_red, LOW);
+      digitalWrite(led_yellow, HIGH);
+      digitalWrite(led_green, LOW);      
+    }
   }
   delay(1);
+  /*
+  Serial.println(distance_1);
+  Serial.println(distance_2);
+  Serial.println(distance_3);
+  Serial.println(distance_4);
+  Serial.println(distance_5);
+  Serial.println("");
+  */
 }
